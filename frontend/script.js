ff098d742d3872c032ed39f4fb9ebf0b2dc8f7a0
abhinavr <abhinavrreji@gmail.com>
2026-06-04 14:10:33 +0530

    first commit

---
+++ frontend/script.js
@@ -0,0 +1,16 @@
+function submitEnquiry(event){
+    event.preventDefault();
+    const name=document.getElementById("name").value.trim();
+    const email=document.getElementById("email").value.trim();
+    const service=document.getElementById("service").value.trim();
+    const message=document.getElementById("message").value.trim();
+    const formMessage=document.getElementById("formMessage");
+
+    if(name===""||email===""||service===""||message===""){
+        formMessage.textContent="Please fill all fields before submitting!!";
+        formMessage.style.color="red";
+        return;
+    }
+    formMessage.textContent = "Thank you, " + name + "! Your enquiry has been recorded for the Day 2 demo.";
+    formMessage.style.color = "#123c69";
+}
